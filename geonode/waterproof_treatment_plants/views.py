b5660124a2e82581ea92b4e8e1775c9d8b8bc884
Yeismer Espejo <yespejo@gmail.com>
2021-03-09 09:31:18 -0500

    treatment_plants_edit.html (add id in the node 12 and 13) treatment_plants_list.html (add js library for alerts and messages) api.py (add documentation for each method) models.py (add new field for id graph in the function entity) views.py (add documentation for each method) 0001_initial.py (add new field for id graph in the function entity) waterproof_list_treatment_plants.js (add documentation for each method)

--- geonode/waterproof_treatment_plants/views.py
+++ geonode/waterproof_treatment_plants/views.py
@@ -6,6 +6,16 @@ import requests
 
 
 def treatmentPlantsList(request):
+	"""Returns the list of treatment plants in view
+
+	Call the api service that looks for the treatment plants and sends them to the treatment plants view
+
+	Parameters:
+	without parameters
+
+	Exceptions:
+	without Exceptions
+	"""
 	if request.method == 'GET':
 		response = requests.get(settings.SITE_HOST_API + 'treatment_plants/getTreatmentPlantsList/')
 		return render(
