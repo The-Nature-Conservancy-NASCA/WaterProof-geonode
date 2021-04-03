3516ea7bac0e561360451038f26ba0f2d4591f9a
Edwin Piragauta Vargas <edwin.piragauta@gmail.com>
2021-04-03 17:25:46 -0500

    fixes in class and attributes notation

--- geonode/waterproof_reports/models.py
+++ geonode/waterproof_reports/models.py
@@ -23,23 +23,21 @@
 
 from django.db import models
 from django.conf import settings
-from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
-from geonode.waterproof_treatment_plants.models import Header , function
-from geonode.waterproof_parameters.models import Countries , Cities , Climate_value
-from geonode.waterproof_study_cases.models import studycases
+from geonode.waterproof_treatment_plants.models import Header, Function
+from geonode.waterproof_parameters.models import Countries, Cities , Climate_value
+from geonode.waterproof_study_cases.models import StudyCases
 from geonode.waterproof_intake.models import ElementSystem 
-from django.db.models.query import QuerySet
 
 
-class wb_Ptap(models.Model):
+class wbPtap(models.Model):
     # Create your models here.
-    study_case_id = models.ForeignKey(studycases, on_delete=models.CASCADE) #Hace referencia al caso de estudio que originó la ejecución del análisis 
+    studycase = models.ForeignKey(StudyCases, on_delete=models.CASCADE) #Hace referencia al caso de estudio que originó la ejecución del análisis 
     stage = models.CharField(max_length=10) #Almacena el escenario para el cual se generaron las variables
-    funtion_plant_id = models.ForeignKey(function, on_delete=models.CASCADE)#hace referencia al elemento del diagrama.
-    function_graph_id = models.IntegerField(verbose_name=_('Graph Id Function'))
+    function_plant = models.ForeignKey(Function, on_delete=models.CASCADE)#hace referencia al elemento del diagrama.
+    function_graph = models.IntegerField(verbose_name=_('Graph Id Function'))
     year = models.IntegerField(verbose_name=_('Year')) 
-    user_id = models.ForeignKey( settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey( settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)
     awy = models.FloatField(null=True, blank=True, default=None, verbose_name=_('Awy'))
     q_l_s = models.FloatField(null=True, blank=True, default=None, verbose_name=_('Qls'))
     cn_mg_l = models.FloatField(null=True, blank=True, default=None, verbose_name=_('Cn'))
@@ -52,17 +50,15 @@ class wb_Ptap(models.Model):
     wp_ret_ton = models.FloatField(null=True, blank=True, default=None, verbose_name=_('WpRetTon'))
     wsed_ret_ton = models.FloatField(null=True, blank=True, default=None, verbose_name=_('WsedRetTon'))
 
-    def __str__(self):
-        return "%s" % self.name
 
-class wb_intake(models.Model):
+class wbIntake(models.Model):
     # Create your models here.
-    study_case_id = models.ForeignKey(studycases, on_delete=models.CASCADE) #Hace referencia al caso de estudio que originó la ejecución del análisis 
+    studycase = models.ForeignKey(StudyCases, on_delete=models.CASCADE) #Hace referencia al caso de estudio que originó la ejecución del análisis 
     stage = models.CharField(max_length=10) #Almacena el escenario para el cual se generaron las variables
-    water_intake_id =models.IntegerField(verbose_name=_('Water intake id')) #hace referencia al elemento del diagrama.
-    element_id = models.IntegerField(verbose_name=_('Element Id'))
+    water_intake =models.IntegerField(verbose_name=_('Water intake id')) #hace referencia al elemento del diagrama.
+    element = models.IntegerField(verbose_name=_('Element Id'))
     year = models.IntegerField(verbose_name=_('Year')) 
-    user_id = models.ForeignKey( settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey( settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)
     awy = models.FloatField(null=True, blank=True, default=None, verbose_name=_('Awy'))
     q_l_s = models.FloatField(null=True, blank=True, default=None, verbose_name=_('Qls'))
     cn_mg_l = models.FloatField(null=True, blank=True, default=None, verbose_name=_('Cn'))
@@ -75,39 +71,32 @@ class wb_intake(models.Model):
     wp_ret_ton = models.FloatField(null=True, blank=True, default=None, verbose_name=_('WpRetTon'))
     wsed_ret_ton = models.FloatField(null=True, blank=True, default=None, verbose_name=_('WsedRetTon'))
 
-    def __str__(self):
-        return "%s" % self.name
 
-class calculate_Function_cost_intake(models.Model):
+class calculateCostFunctionIntake(models.Model):
     # Create your models here.
-    study_case_id = models.ForeignKey(studycases, on_delete=models.CASCADE)
+    study_case = models.ForeignKey(StudyCases, on_delete=models.CASCADE)
     stage = models.CharField(max_length=10)	
-    water_intake_id =models.IntegerField(verbose_name=_('Water intake id')) #hace referencia al elemento del diagrama.
-    element_id = models.IntegerField(verbose_name=_('Element Id'))
-    user_id = models.ForeignKey( settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)	
+    water_intake =models.IntegerField(verbose_name=_('Water intake id')) #hace referencia al elemento del diagrama.
+    element = models.IntegerField(verbose_name=_('Element Id'))
+    user = models.ForeignKey( settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)	
     year = models.IntegerField(verbose_name=_('Year'))	
-    value_calculate = models.FloaField(null=True, blank=True, default=0, verbose_name=_('Value calculate'))	
-    Function_cost_id = models.IntegerField(verbose_name=_('Graph Id Function'))	
+    value_calculate = models.FloatField(null=True, blank=True, default=0, verbose_name=_('Value calculate'))	
+    cost_function = models.IntegerField(verbose_name=_('Graph Id Function'))	
     currency_function = models.CharField(max_length=4, blank=True, null=True)
     date_excution = models.DateTimeField(blank=True, null=True)
 
-    def __str__(self):
-        return "%s" % self.name
 
-class calculate_Function_cost_Ptap(models.Model):
+class calculateCostFunctionPtap(models.Model):
     # Create your models here.
-    study_case_id = models.ForeignKey(studycases, on_delete=models.CASCADE)
+    study_case = models.ForeignKey(StudyCases, on_delete=models.CASCADE)
     stage = models.CharField(max_length=10)	
-    funtion_plant_id = models.ForeignKey(function, on_delete=models.CASCADE)#hace referencia al elemento del diagrama.
-    function_graph_id = models.IntegerField(verbose_name=_('Graph Id Function'))
-    user_id = models.ForeignKey( settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)	
+    function_plant = models.ForeignKey(Function, on_delete=models.CASCADE)#hace referencia al elemento del diagrama.
+    function_graph = models.IntegerField(verbose_name=_('Graph Id Function'))
+    user = models.ForeignKey( settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)	
     year = models.IntegerField(verbose_name=_('Year'))	
-    value_calculate = models.FloaField(null=True, blank=True, default=0, verbose_name=_('Value calculate'))	
-    Function_cost_id = models.IntegerField(verbose_name=_('Graph Id Function'))	
+    value_calculate = models.FloatField(null=True, blank=True, default=0, verbose_name=_('Value calculate'))	
+    cost_function = models.IntegerField(verbose_name=_('Graph Id Function'))	
     currency_function = models.CharField(max_length=4, blank=True, null=True)
     date_excution = models.DateTimeField(blank=True, null=True)
-
-    def __str__(self):
-        return "%s" % self.name
 
 
