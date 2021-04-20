c7330625d1003a4044cc419f030b0700887d0cd5
Edwin Piragauta <edwin.piragauta@gmail.com>
2021-04-20 16:38:54 -0500

    Enable load initial data using sql

--- scripts_docker/spcwaterproof/django/docker-entrypoint.sh
+++ scripts_docker/spcwaterproof/django/docker-entrypoint.sh
@@ -32,6 +32,15 @@ export GEOSERVER_PUBLIC_LOCATION="${SITEURL}/geoserver/"
 echo 'Running initialize.py...'
 python -u scripts_docker/spcwaterproof/django/initialize.py
 
+echo "Running sql scripts... '$GEONODE_DATABASE_USER' :: '$GEONODE_DATABASE'"
+export PGPASSWORD="${GEONODE_DATABASE_PASSWORD}"
+psql -U $GEONODE_DATABASE_USER -d $GEONODE_DATABASE -h postgres -f initial_data/waterproof_tbls-parameters-models.sql
+psql -U $GEONODE_DATABASE_USER -d $GEONODE_DATABASE -h postgres -f initial_data/waterproof_parameters_countries.sql
+psql -U $GEONODE_DATABASE_USER -d $GEONODE_DATABASE -h postgres -f initial_data/waterproof_parameters_cities.sql
+psql -U $GEONODE_DATABASE_USER -d $GEONODE_DATABASE -h postgres -f initial_data/waterproof_functions.sql
+
+ogr2ogr -append --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -nln public.waterproof_intake_basins -f "PostgreSQL" PG:"dbname=geonode host=postgres user=geonode password=G30N0D3" -a_srs EPSG:4326 initial_data/basins.gpkg waterproof_intake_basins
+
 echo "-----------------------------------------------------"
 echo "FINISHED DJANGO ENTRYPOINT --------------------------"
 echo "-----------------------------------------------------"
