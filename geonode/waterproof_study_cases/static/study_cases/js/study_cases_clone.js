107ae25a5b83c36685400930354791cc4dee364c
Edwin Alberto Piragauta Vargas <edwin.piragauta@gmail.com>
2021-10-04 10:28:37 -0500

    Change message Save to Confirm and run
    Signed-off-by: Edwin Alberto Piragauta Vargas <edwin.piragauta@gmail.com>

--- geonode/waterproof_study_cases/static/study_cases/js/study_cases_clone.js
+++ geonode/waterproof_study_cases/static/study_cases/js/study_cases_clone.js
@@ -607,7 +607,7 @@ $(document).ready(function () {
                         title: gettext('exchange_rate'),
                         html: html,
                         showCancelButton: true,
-                        confirmButtonText: gettext('Save'),
+                        confirmButtonText: gettext('Confirm and run'),
                         preConfirm: () => {
                             currencys = []
                             $("#currencys-panel").find("input").each(function (index, input) {
