c79d21fedc065ee8804cb52ffe67c796d1c201a4
ariascoss <ariascoss@gmail.com>
2021-10-09 10:52:40 -0500

    page 5 change image III

--- geonode/waterproof_reports/views.py
+++ geonode/waterproof_reports/views.py
@@ -555,9 +555,9 @@ def pdf(request):
     }
 
     hc_export.save_as_png(config=config, filename="imgpdf/cab.png")
-    pdf.image('imgpdf/cab.png', 35, 30, w=120, h=90, type='png')
+    pdf.image('imgpdf/cab.png', 35, 30, w=100, h=60, type='png')
 
-    pdf.ln(50)
+    pdf.ln(80)
     pdf.set_font('Arial', '', 13)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(0, 10, 'Comparative chart of costs and benefits:', align='L')
