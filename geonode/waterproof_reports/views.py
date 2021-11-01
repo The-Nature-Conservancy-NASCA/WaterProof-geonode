f26c30dec217b637b077d041c020ce3cd33b3d81
ariascoss <ariascoss@gmail.com>
2021-11-01 11:08:06 -0500

    Change pdf path

--- geonode/waterproof_reports/views.py
+++ geonode/waterproof_reports/views.py
@@ -1922,7 +1922,7 @@ def pdf(request):
         heightIcon = heightIcon + 80;
 
 #    response = HttpResponse(pdf.output(dest='S').encode('iso-8859-1'))
-    StudyCaseNum = 'Case_study_' + request.POST['studyCase']+'.pdf';
+    StudyCaseNum = './Case_study_' + request.POST['studyCase']+'.pdf';
     response = HttpResponse(pdf.output('D',StudyCaseNum).encode('iso-8859-1'))
 
     response['Content-Type'] = 'application/pdf'
