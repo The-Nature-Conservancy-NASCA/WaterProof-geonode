b0bc901b45fe0e01a0ac0fc60d8dcf8405276534
Laura_Skaphe <lauragonsan7@gmail.com>
2021-12-01 09:54:21 -0500

    Revert "ajuste descarga de pdf visualizacion geografica"
    This reverts commit 5f611b0ef15f0da78171b160db60a320fbfd4b6a.

--- geonode/waterproof_reports/static/waterproof_reports/js/reports_maps.js
+++ geonode/waterproof_reports/static/waterproof_reports/js/reports_maps.js
@@ -5,18 +5,17 @@
 
 $(document).ready(function () {
   openPDF = function() {      
-      generateMap2Img('mapidcuenca','resultado');            
+    generateMap2Img('resultado');
+    setTimeout(function(){
       document.getElementById('studyCase').value = localStorage.idStudyCase;
       document.getElementById('studyCity').value = localStorage.getItem('city');
       document.getElementById('studyCountry').value = localStorage.getItem('country');
       document.getElementById('studyRegion').value = localStorage.getItem('region');
-      document.getElementById('discountRateData').value = document.getElementById("discountRateGeographic").innerHTML;
-          
-      setTimeout(function(){
-          window.open('', 'TheWindow');
-          document.getElementById('TheForm').submit();
-      }, 2000);
-  }
+      document.getElementById('discountRateData').value = document.getElementById("discountRate").innerHTML;
+      window.open('', 'TheWindow');
+      document.getElementById('TheForm').submit();
+    }, 4000);
+  } 
 
   var urlDetail = "../../reports/getSelectorStudyCasesId/?studyCase=" + studyCaseId;
   var selectIntake = document.getElementById("idSelectStudyCase");
